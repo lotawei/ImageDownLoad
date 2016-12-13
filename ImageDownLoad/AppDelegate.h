26f1071313554dbc9e1eec641db6dd74cce4d77d
lwiosbystep <1286594907@qq.com>
2016-12-13 17:18:56 +0800

    Initial Commit

---
+++ ImageDownLoad/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImageDownLoad
+//
+//  Created by lotawei on 16/12/13.
+//  Copyright © 2016年 lotawei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ImageDownLoad/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImageDownLoad
+//
+//  Created by lotawei on 16/12/13.
+//  Copyright © 2016年 lotawei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
